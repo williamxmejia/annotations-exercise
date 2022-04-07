dac175f29172c154f2949e86f91c8b6eee597b67
William Mejia <willx1995@gmail.com>
2022-04-07 11:00:05 -0500

    Complete better fix

--- src/Author.java
+++ src/Author.java
@@ -4,7 +4,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-@SuppressWarnings("unchecked")
+//@SuppressWarnings("unchecked")
 public class Author extends Person {
     private List books;
 
